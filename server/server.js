b7810d536d79b9611c3b91d7531de88aedf463bf
harsha <sriharshashm@gmail.com>
2018-07-29 01:14:10 +0530

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,14 +14,36 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
     
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'Well, see you then',
-        createdAt: 1231411
+    // socket.emit from admin text welcome to chat app
+
+    socket.emit('newMessage',{
+        from: 'Admin',
+        text: 'Welcome to chat app',
+        createdAt: new Date().getTime()
     });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
+    // socket.broadcast.emit from Admin text New user joined
+
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+        // Broadcasting events
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text:message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () =>{
